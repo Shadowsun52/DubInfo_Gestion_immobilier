4942521777164f700f01d8c4e1dc968210c71041
Alexandre <jenicot.alexandre@gmail.com>
2015-04-08 10:05:05 +0200

    Ajout du model : VisiteMaison

---
+++ model/VisiteMaison.php
@@ -0,0 +1,39 @@
+<?php
+namespace DubInfo_gestion_immobilier\model;
+
+use DubInfo_gestion_immobilier\Exception\StringAttributeTooLong;
+use DubInfo_gestion_immobilier\Exception\BadTypeException;
+/**
+ * Description of VisiteMaison
+ *
+ * @author Jenicot Alexandre
+ */
+abstract class VisiteMaison extends Visite{
+    /**
+     *
+     * @var Maison 
+     */
+    private $_maison;
+    
+    public function __construct($id = NULL, $date = NULL, $rapport = NULL, 
+            $maison = NULL, $participants = NULL) {
+        parent::__construct($id, $date, $rapport, $participants);
+        $this->setMaison($maison);
+    }
+    
+    /**
+     * 
+     * @return Maison
+     */
+    public function getMaison() {
+        return $this->_maison;
+    }
+    
+    /**
+     * 
+     * @param Maison $maison
+     */
+    public function setMaison($maison) {
+        $this->_maison = CheckTyper::isModel($maison, Maison::class, 'maison', __CLASS__);
+    }
+}
