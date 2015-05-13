b28947282e412cf3b66c6fef066d4901c3750c1c
Alexandre <jenicot.alexandre@gmail.com>
2015-05-13 16:18:09 +0200

    Ajout de la génération excel pour projet

---
+++ excel/SheetExcel_projet.php
@@ -0,0 +1,43 @@
+<?php
+namespace DubInfo_gestion_immobilier\excel;
+
+/**
+ * Description of SheetExcel_projet
+ *
+ * @author Jenicot Alexandre
+ */
+class SheetExcel_projet extends SheetExcel{
+    
+    /**
+     * Méthode pour définir la taille des colonnes
+     */
+    protected function setColWidth() {
+        $this->getSheet()->getColumnDimension('A')->setWidth(20);
+        $this->getSheet()->getColumnDimension('B')->setWidth(20);
+        $this->getSheet()->getColumnDimension('C')->setWidth(30);
+        $this->getSheet()->getColumnDimension('D')->setWidth(20);
+        $this->getSheet()->getColumnDimension('E')->setWidth(20);
+        $this->getSheet()->getColumnDimension('F')->setWidth(10);
+        $this->getSheet()->getColumnDimension('G')->setWidth(10);
+        $this->getSheet()->getColumnDimension('H')->setWidth(10);
+        $this->getSheet()->getColumnDimension('I')->setWidth(45);
+    }
+
+    /**
+     * Fonction qui retourne le nom des colonnes de la table
+     * @return array[string]
+     */
+    public function getNameColumns() {
+        return ['Nom', 'Etat', 'Maison', 'Date compromis', 'Date acte', 
+            'Plan métré', 'Devis entrepreneur', 'Sélection des matériaux', 
+            'Remarques'];
+    }
+
+    /**
+     * Fonction qui retourne la partie personnalisé du titre du document
+     * @return string 
+     */
+    public function getDocTitle() {
+        return 'Liste des projets';
+    }
+}
